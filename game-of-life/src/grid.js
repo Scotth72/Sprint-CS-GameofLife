4d6de579ac17b63f9b743cfecd54d1d751b60eab
Heath Scott <you@example.com>
2020-06-25 22:39:35 -0500

    Grid is made, looking good

---
+++ game-of-life/src/grid.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+
+// class Grid extends React.Component {
+//     render() {
+//         return (
+//             <div>
+//                 Grid
+//             </div>
+//         )
+//     }
+// }
+
+// export default Grid;
